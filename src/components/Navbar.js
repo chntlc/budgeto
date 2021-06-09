029f379e9bb2fe4694efbb62c6390ffb48def34a
tmdenddl <seungwook.l95@gmail.com>
2021-06-08 18:29:39 -0700

    Skeleton Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import NavbarList from './NavbarList'
 import '../css/Navbar.css';
+import CoinImage from '../images/coin.png';
 
-function Navbar() {
+function Navbar(props) {
   return(
     <div className="navbar">
-
+      <div className="logo">
+        <div className="helper"></div><div className="vertical-center">
+          <img src={CoinImage} width="34" height="34" alt="Coin" />
+          <p>broke.io</p>
+        </div>
+      </div>
       <ul className="menu-right">
-        <NavbarList button="Home" />
-        <NavbarList button="Login" />
+        <NavbarList button={props.button} />
       </ul>
     </div>
   );
