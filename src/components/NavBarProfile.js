a5e6337d4afcd1ff3e523f1b67af52202b23ff3c
Thomas Kwun <taekheekwun@gmail.com>
2021-06-11 14:31:23 -0700

    profile dropdown

--- src/components/NavBarProfile.js
+++ src/components/NavBarProfile.js
@@ -1,46 +1,44 @@
 import React from "react";
-// import { Menu, Form, Input, Button, Dropdown } from "antd";
+import "antd/dist/antd.css";
+import { Menu, Form, Input, Button, Dropdown } from "antd";
 
-// const menu = () => {
-//   const layout = {
-//     labelCol: {
-//       span: 8,
-//     },
-//     wrapperCol: {
-//       span: 16,
-//     },
-//   };
-//   const tailLayout = {
-//     wrapperCol: {
-//       offset: 16,
-//       span: 8,
-//     },
-//   };
-//   return (
-//     <Menu>
-//       <Menu.Item key="0">
-//         <Form {...layout}>
-//           <Form.Item label="Weekly Budget" name="weeklyBudget">
-//             <Input></Input>
-//           </Form.Item>
-//           <Form.Item {...tailLayout}>
-//             <Button type="primary" htmlType="submit">
-//               Set
-//             </Button>
-//           </Form.Item>
-//         </Form>
-//       </Menu.Item>
-//     </Menu>
-//   );
-// };
+const menu = () => {
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 18,
+    },
+  };
+  return (
+    <Menu>
+      <Menu.Item key="0">
+        <Form {...layout}>
+          <Form.Item label="Weekly Budget" name="weeklyBudget">
+            <Input></Input>
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Set
+            </Button>
+          </Form.Item>
+        </Form>
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 function NavBarProfile(props) {
   return (
-    // <Dropdown overlay={menu} placement="bottomRight" trigger={["click"]}>
-    //   <a onClick={(e) => e.preventDefault()}>
-    <img src={props.imgUrl} alt="" className="navbar__profile" />
-    //   </a>
-    // </Dropdown>
+    <Dropdown overlay={menu} placement="bottomRight" trigger={["click"]} arrow>
+      <img src={props.imgUrl} alt="" className="navbar__profile" />
+    </Dropdown>
   );
 }
 
