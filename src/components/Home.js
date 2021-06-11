3702af6e8ded50356bd5d8f094e730874ed0d72b
tmdenddl <seungwook.l95@gmail.com>
2021-06-10 22:06:04 -0700

    Basic word animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import TextTransition, { presets } from "react-text-transition";
+
 import '../css/Home.css';
 
 function Home() {
+  const TEXTS = [
+    "See More",
+    "Spend Less",
+    "Start Saving"
+  ];
+
+  const [index, setIndex] = React.useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() =>
+      setIndex(index => index + 1),
+      2000 // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
   return(
     <section className="home-section">
       <div className="helper"></div><div className="vertical-center">
-        <h1>This is the Landing Page</h1>
-        <p>We need more details here!</p>
+        <div className="welcome-text">
+          <h1>
+            <TextTransition
+              text={ TEXTS[index % TEXTS.length] }
+              springConfig={ presets.wobbly }
+            />
+          </h1>
+        </div>
       </div>
     </section>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+
 import '../css/Login.css';
 
 function Login() {
