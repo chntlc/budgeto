3bd81f4619c7854a764383588cce5400cc1ca2fc
Thomas Kwun <taekheekwun@gmail.com>
2021-06-11 03:28:26 -0700

    tailor navbar to include profile after login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import NavbarList from './NavbarList'
-import '../css/Navbar.css';
-import CoinImage from '../images/coin.png';
+import React from "react";
+import NavbarList from "./NavbarList";
+import "../css/Navbar.css";
+import CoinImage from "../images/coin.png";
 
 function Navbar(props) {
-  return(
+  return (
     <div className="navbar">
       <div className="logo">
-        <div className="helper"></div><div className="vertical-center">
+        <div className="helper"></div>
+        <div className="vertical-center">
           <img src={CoinImage} width="34" height="34" alt="Coin" />
           <p>broke.io</p>
         </div>
       </div>
       <ul className="menu-right">
-        <NavbarList button={props.button} />
+        <NavbarList loggedIn={props.loggedIn} />
       </ul>
     </div>
   );
