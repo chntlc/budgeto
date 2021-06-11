fb6ebf9f5c52bdd06b08383a87ceb11102cfc72a
tmdenddl <seungwook.l95@gmail.com>
2021-06-10 19:25:01 -0700

    Changed the Navbar style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Navbar.css';
 import CoinImage from '../images/coin.png';
+import Logo from '../images/logo.png';
 import NavigationButton from './NavigationButton'
 
 function Navigation(props) {
@@ -10,8 +11,8 @@ function Navigation(props) {
     <div className="navbar">
       <div className="logo">
         <div className="helper"></div><div className="vertical-center">
-          <img src={CoinImage} width="34" height="34" alt="Coin" />
-          <p>broke.io</p>
+          <img className="logo-img" src={Logo} width="34" height="34" alt="Coin" />
+          <h2>BUDGETO</h2>
         </div>
       </div>
       <div className="menu-right">
