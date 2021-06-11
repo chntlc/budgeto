80739d18ca0dfb1e7e4b9c8980c5aaf633d72189
tmdenddl <seungwook.l95@gmail.com>
2021-06-10 19:25:01 -0700

    Changed the Navbar style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 function Login() {
   return(
     <section>
+      <Header pages={["Home"]} />
       <div className="login">
         <h1>Need to remove empty space at top</h1>
         <h1>This is the Login Page</h1>
