f5aacb17975acbeb6d322cdce17db7bcc348b39a
kouitayakul <kouitayakul@gmail.com>
2021-06-11 23:15:57 -0700

    Adding a route to ReceiptUploadedPage component

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,8 @@ import Home from "./Home";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import ViewPage from "./ViewPage";
-import LoginSignup from './LoginSignup';
+import LoginSignup from "./LoginSignup";
+import ReceiptUploadedPage from "./ReceiptUploadedPage/ReceiptUploadedPage";
 
 function Router(props) {
   // How to go back to previous page: https://stackoverflow.com/questions/30915173/react-router-go-back-a-page-how-do-you-configure-history
@@ -23,6 +24,7 @@ function Router(props) {
           <Route path="/login" component={Login} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/view" component={ViewPage} />
+          <Route path="/receiptUploaded" component={ReceiptUploadedPage} />
         </Switch>
       </div>
     </BrowserRouter>
