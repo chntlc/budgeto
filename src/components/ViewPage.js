18d6d3eb3ff8293c4f688461d5e28a41f280fdf2
Thomas Kwun <taekheekwun@gmail.com>
2021-06-11 17:20:40 -0700

    view page, calendar

---
+++ src/components/ViewPage.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Calendar } from "antd";
+import "../css/ViewPage.css";
+import "antd/dist/antd.css";
+
+class ViewPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewType: "default",
+    };
+    this.setTypeCustom = this.setTypeCustom.bind(this);
+    this.setTypeDefault = this.setTypeDefault.bind(this);
+  }
+
+  setTypeCustom() {
+    this.setState({
+      viewType: "custom",
+    });
+  }
+
+  setTypeDefault() {
+    this.setState({
+      viewType: "default",
+    });
+  }
+
+  render() {
+    const { viewType } = this.state;
+    return (
+      <div className="viewpage page-content">
+        <div className="viewpage__typeChoice">
+          <button
+            className={viewType === "default" ? "current" : null}
+            onClick={this.setTypeDefault}
+          >
+            Default Calendar
+          </button>
+          <button
+            className={viewType === "custom" ? "current" : null}
+            onClick={this.setTypeCustom}
+          >
+            Custom Range
+          </button>
+        </div>
+        <hr />
+        {viewType === "default" ? <Calendar /> : null}
+      </div>
+    );
+  }
+}
+
+export default ViewPage;
