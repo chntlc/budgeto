b89305d817a332e1b55067a373f88c240a58a053
kouitayakul <kouitayakul@gmail.com>
2021-06-11 23:14:28 -0700

    Adding category view aligning category picker and category filter

--- src/components/ReceiptUploadedPage/CategoryFilter.js
+++ src/components/ReceiptUploadedPage/CategoryFilter.js
@@ -1,6 +1,8 @@
 import "./CategoryFilter.css";
 
-function CategoryFilter() {
+function CategoryFilter(props) {
+  let categories = props.categories;
+
   function openFilterContainer(event) {
     event.target.classList.toggle("active");
 
@@ -13,37 +15,31 @@ function CategoryFilter() {
   }
 
   return (
-    <div>
-      <div className="filterContainer">
-        <button
-          type="button"
-          className="collapsible"
-          onClick={(event) => openFilterContainer(event)}
-        ></button>
-        <div className="filteredItems">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-      </div>
-      <div className="filterContainer">
-        <button
-          type="button"
-          className="collapsible"
-          onClick={(event) => openFilterContainer(event)}
-        ></button>
-        <div className="filteredItems">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-      </div>
+    <div className="filterContainer">
+      {categories.map((category, index) => {
+        if (index !== categories.length - 1) {
+          return (
+            <div className="filter" key={index}>
+              <button
+                type="button"
+                className="collapsible"
+                style={{ backgroundColor: category["iconColor"] }}
+                onClick={(event) => openFilterContainer(event)}
+              >
+                {category["category"]}
+              </button>
+              <div className="filteredItems">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
--- src/components/ReceiptUploadedPage/CategoryPicker.js
+++ src/components/ReceiptUploadedPage/CategoryPicker.js
@@ -1,39 +1,8 @@
 import Category from "./Category";
-import GroceryIcon from "../../images/groceryIcon.png";
-import RestaurantIcon from "../../images/restaurantIcon.png";
-import ClothingIcon from "../../images/clothingIcon.png";
-import TransportationIcon from "../../images/transportationIcon.png";
-import AddIcon from "../../images/addIcon.png";
 import "./CategoryPicker.css";
 
-function CategoryPicker() {
-  let categories = [
-    {
-      category: "Grocery",
-      iconImg: GroceryIcon,
-      iconColor: "#81B214",
-    },
-    {
-      category: "Dining Out",
-      iconImg: RestaurantIcon,
-      iconColor: "#F54748",
-    },
-    {
-      category: "Clothing",
-      iconImg: ClothingIcon,
-      iconColor: "#FFC93C",
-    },
-    {
-      category: "Transportation",
-      iconImg: TransportationIcon,
-      iconColor: "#AC66CC",
-    },
-    {
-      category: "Add More",
-      iconImg: AddIcon,
-      iconColor: "#A7BBC7",
-    },
-  ];
+function CategoryPicker(props) {
+  let categories = props.categories;
 
   return (
     <div className="categoryContainer">
