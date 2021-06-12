f7cb05b693135b8b1c7b895130a063cd92f7fbe9
chntlc <chantaalchung@gmail.com>
2021-06-12 09:25:56 -0700

    add login to start saving button, edit styling for readability

--- src/components/StartSaving.js
+++ src/components/StartSaving.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import LoginSignup from './LoginSignup';
 
 
 import '../css/Home.css';
 
 function StartSaving() {
+  const [showLoginSignup, setLoginSignup] = useState(false)
 
   return (
-    <div className="see-more">
-      <NavLink className="start-saving" to="/login">Start Saving</NavLink>
-    </div>
+    <React.Fragment>
+      {showLoginSignup && <LoginSignup />}
+      <div className="see-more">
+        <button className='start-saving' onClick={() => {
+          setLoginSignup(true)
+        }}>Start Saving</button>
+        {/* <NavLink className="start-saving" to="/login">Start Saving</NavLink> */}
+      </div>
+    </React.Fragment>
   );
 }
 
