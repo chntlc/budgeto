afc9b807dd8105eca48c18a7abd73f4be10ede50
tmdenddl <seungwook.l95@gmail.com>
2021-06-24 00:18:06 -0700

    Migrate localUser into global store

--- src/app/store.js
+++ src/app/store.js
@@ -2,24 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import categorySlice from '../features/categorySlice'
 import globalSlice from '../features/globalSlice'
 import receiptSlice from '../features/receiptSlice'
-import settingsSlice from '../features/settingsSlice'
 
 export default configureStore({
   reducer: {
     global: globalSlice,
     receipt: receiptSlice,
-    category: categorySlice,
-    settings: settingsSlice,
+    category: categorySlice
   },
 })
-
-// const store = configureStore({
-//   reducer: {
-//     global: globalSlice,
-//     receipt: receiptSlice,
-//     category: categorySlice,
-//     settings: settingsSlice,
-//   },
-// });
-//
-// export default store;
--- src/features/globalSlice.js
+++ src/features/globalSlice.js
@@ -11,6 +11,13 @@ const globalSlice = createSlice({
       budget: 0,
       email: 'budgeto@gmail.com',
     },
+    localUser: {
+      id: 'budgeto.io',
+      fname: 'Kevin',
+      lname: 'Lee',
+      budget: 0,
+      email: 'budgeto@gmail.com',
+    },
     showLoginModal: '',  // can be: login, signup, or ''
     showSettingsModal: '' // can be: settings or ''
   },
@@ -25,11 +32,37 @@ const globalSlice = createSlice({
     updateUser: (state, action) => {
       console.log('hit updateUser action')
 
+      state.user.id = action.payload.id;
       state.user.fname = action.payload.fname;
       state.user.lname = action.payload.lname;
       state.user.budget = action.payload.budget;
       state.user.email = action.payload.email;
-      state.showSettingsModal = 'settings';
+      state.showSettingsModal = '';
+    },
+    updateLocalUser: (state, action) => {
+      console.log('hit updateLocalUser action')
+
+      let inputId = action.payload.id
+      let value = action.payload.value
+      switch(inputId) {
+        case "id":
+          state.localUser.id = value;
+          break;
+        case "fname":
+          state.localUser.fname = value;
+          break;
+        case "lname":
+          state.localUser.lname = value;
+          break;
+        case "email":
+          state.localUser.email = value;
+          break;
+        case "budget":
+          state.localUser.budget = value;
+          break;
+        default:
+          break;
+      }
     },
     toggleLoginModal: (state, action) => {
       console.log('hit toggleLoginModal action')
@@ -42,6 +75,6 @@ const globalSlice = createSlice({
   }
 })
 
-export const { userLogin, userLogout, updateUser, toggleLoginModal, toggleSettingsModal } = globalSlice.actions
+export const { userLogin, userLogout, updateUser, updateLocalUser, toggleLoginModal, toggleSettingsModal } = globalSlice.actions
 
 export default globalSlice.reducer
