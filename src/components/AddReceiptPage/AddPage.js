727b96dbcc45587627e007f26284854374a759f8
chntlc <chantaalchung@gmail.com>
2021-06-25 01:19:59 -0700

    handle update items in form

--- src/components/AddReceiptPage/AddPage.js
+++ src/components/AddReceiptPage/AddPage.js
@@ -1,24 +1,85 @@
 import React, { useState } from 'react'
 import './AddPage.css'
 import { Card, Col, Row } from 'antd';
+import { Link } from "react-router-dom";
+import { connect, useDispatch } from 'react-redux'
+import { addItems } from '../../features/receiptSlice'
 
-function AddPage() {
-  const [items, setItems] = useState([])
+function AddPage(props) {
+  const dispatch = useDispatch()
+  const [inputRows, setInputRows] = useState([])
+  const [items, setItems] = useState([{
+    name: '',
+    qty: '',
+    price: ''
+  }])
 
   const addInputRow = () => {
     console.log('add input row')
-    setItems([...items, {
-      name: '',
-      qty: '',
-      price: ''
-    }])
+    // setItems([...items, {
+    //   name: '',
+    //   qty: '',
+    //   price: ''
+    // }])
+    setInputRows([...inputRows, {}])
     console.log({ items })
+  }
+
+  const handleAddItems = () => {
+    dispatch(addItems(items))
+  }
+
+  const handleNameInput = (value, index) => {
+    // 1. Make a shallow copy of the items
+    let itemsCopy = [...items]
+    // 2. Make a shallow copy of the item you want to mutate
+    let item = { ...itemsCopy[index] }
+    // 3. Replace the property you're intested in
+    item.name = value
+    // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
+    itemsCopy[index] = item;
+    // 5. Set the state to our new copy
+    setItems(itemsCopy);
+  }
+
+  const handleQtyInput = (value, index) => {
+    // 1. Make a shallow copy of the items
+    let itemsCopy = [...items]
+    // 2. Make a shallow copy of the item you want to mutate
+    let item = { ...itemsCopy[index] }
+    // 3. Replace the property you're intested in
+    item.qty = value
+    // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
+    itemsCopy[index] = item;
+    // 5. Set the state to our new copy
+    setItems(itemsCopy);
+  }
+
+  const handlePriceInput = (value, index) => {
+    // 1. Make a shallow copy of the items
+    let itemsCopy = [...items]
+    // 2. Make a shallow copy of the item you want to mutate
+    let item = { ...itemsCopy[index] }
+    // 3. Replace the property you're intested in
+    item.price = value
+    // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
+    itemsCopy[index] = item;
+    // 5. Set the state to our new copy
+    setItems(itemsCopy);
   }
 
   // TODO: add button that takes all items in state, when clicked, adds to our store in receiptSlice
 
   return (
     <div>
+      <Row gutter={16} justify="end" className='button-row'>
+        <Col className='col-content'>
+          {/* <button className='next-button'>Next</button> */}
+          <Link to='/receiptUploaded' className='next-button' onClick={handleAddItems}>
+            Next
+          </Link>
+        </Col>
+      </Row>
       <Row gutter={16} className='row-content'>
         <Col flex={2} className='col-content'>
           <Card title='Upload a Receipt' className='card'>
@@ -31,30 +92,30 @@ function AddPage() {
               <Row gutter={16} className='input-row'>
                 <Col span={16}>
                   <label>Name</label>
-                  <input className='item-name-input' type='text' />
+                  <input className='item-name-input' type='text' onChange={(e) => handleNameInput(e.target.value, 0)} />
                 </Col>
                 <Col span={4}>
                   <label>Quantity</label>
-                  <input className='item-qty-input' type='number' />
+                  <input className='item-qty-input' type='number' onChange={(e) => handleQtyInput(e.target.value, 0)} />
                 </Col>
                 <Col span={4}>
                   <label>Price</label>
-                  <input className='item-price-input' type='number' />
+                  <input className='item-price-input' type='number' onChange={(e) => handlePriceInput(e.target.value, 0)} />
                 </Col>
               </Row>
               {
-                items.map((item, index) => {
-                  let itemId = `item-${index}`
+                inputRows.map((item, index) => {
+                  let itemId = `item-${index + 1}`
                   return (
-                    <Row gutter={16} className='input-row'>
+                    <Row key={itemId + '-row'} gutter={16} className='input-row'>
                       <Col span={16}>
-                        <input className='item-name-input' type='text' />
+                        <input key={itemId + '-name'} className='item-name-input' type='text' onChange={(e) => handleNameInput(e.target.value, index + 1)} />
                       </Col>
                       <Col span={4}>
-                        <input className='item-qty-input' type='number' />
+                        <input key={itemId + '-qty'} className='item-qty-input' type='number' onChange={(e) => handleQtyInput(e.target.value, index + 1)} />
                       </Col>
                       <Col span={4}>
-                        <input className='item-price-input' type='number' />
+                        <input key={itemId + '-price'} className='item-price-input' type='number' onChange={(e) => handlePriceInput(e.target.value, index + 1)} />
                       </Col>
                     </Row>
                   )
@@ -69,4 +130,10 @@ function AddPage() {
   )
 }
 
-export default AddPage
+const mapStateToProps = (state) => {
+  return {
+    items: state.receipt.items
+  }
+}
+
+export default connect(mapStateToProps)(AddPage)
