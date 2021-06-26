2fd475fe5062cb7cabb4d95bd82bfa5e0a0e0f39
Thomas Kwun <taekheekwun@gmail.com>
2021-06-26 01:46:01 -0700

    report page & some fixes for view page

--- src/components/ReportPage.js
+++ src/components/ReportPage.js
@@ -9,48 +9,115 @@ class ReportPage extends React.Component {
     super();
   }
 
+  isSingleDayReport() {
+    const { mode, calendarMode, periodStart, periodEnd } = this.props;
+    return (
+      (mode === "calendar" && calendarMode === "month") ||
+      (mode === "custom-range" &&
+        periodStart.format("YYYY-MM-DD") === periodEnd.format("YYYY-MM-DD"))
+    );
+  }
+
+  dateTextHelper() {
+    const { mode, selectedDate, selectedMonth, periodStart, periodEnd } =
+      this.props;
+    if (mode === "calendar") {
+      return this.isSingleDayReport()
+        ? selectedDate.format("YYYY-MM-DD")
+        : `${selectedMonth
+            .startOf("month")
+            .format("YYYY-MM-DD")} ~ ${selectedMonth
+            .endOf("month")
+            .format("YYYY-MM-DD")}`;
+    } else {
+      return this.isSingleDayReport()
+        ? periodStart.format("YYYY-MM-DD")
+        : `${periodStart.format("YYYY-MM-DD")} ~ ${periodEnd.format(
+            "YYYY-MM-DD"
+          )}`;
+    }
+  }
+
   render() {
-    const data = {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    const pieOptions = {
+      plugins: {
+        labels: {
+          render: "percentage",
+          fontColor: "black",
+          precision: 2,
+        },
+        legend: {
+          position: "bottom",
+        },
+      },
+
+      maintainAspectRatio: false,
+    };
+
+    const pieData = {
+      labels: ["Food", "Groceries", "Entertainment", "etc."],
       datasets: [
         {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
+          label: "Spendings per Category ($)",
+          data: [12, 15, 7, 3],
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
+            "rgba(54, 162, 235, 0.5)",
+            "rgba(175, 42, 42, 0.5)",
+            "rgba(153, 102, 255, 0.5)",
+            "rgba(255, 206, 86, 0.5)",
           ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
+          hoverOffset: 4,
         },
       ],
     };
+
+    const lineData = {
+      labels: ["1", "2", "3", "4", "5", "6"],
+      datasets: [
+        {
+          label: "Spendings",
+          data: [12, 19, 3, 5, 2, 3],
+          fill: false,
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgba(255, 99, 132, 0.2)",
+        },
+      ],
+    };
+    const lineOptions = {
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    };
+
     return (
       <div className="reportpage page-content">
-        <Carousel dotPosition="right">
-          <div>
-            <Pie data={data} />
+        <span className="reportpage__title">
+          Here is your report ({this.dateTextHelper()}):
+        </span>
+        {this.isSingleDayReport() ? (
+          <div className="reportpage__chartArea">
+            <Pie data={pieData} options={pieOptions} />
           </div>
-          {(this.props.mode === "calendar" &&
-            this.props.calendarMode === "year") ||
-          (this.props.mode === "custom-range" &&
-            this.props.periodStart !== this.props.periodEnd) ? (
-            <div>
-              <Line />
+        ) : (
+          <Carousel dotPosition="right">
+            <div className="reportpage__chartArea">
+              <Pie data={pieData} options={pieOptions} />
             </div>
-          ) : null}
-        </Carousel>
+
+            <div className="reportpage__chartArea">
+              <div>
+                <Line data={lineData} options={lineOptions} />
+              </div>
+            </div>
+          </Carousel>
+        )}
       </div>
     );
   }
