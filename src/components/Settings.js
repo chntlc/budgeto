eb34cff66465219322c955a40aee6f8b059d028e
tmdenddl <seungwook.l95@gmail.com>
2021-06-25 17:59:26 -0700

    Moved localUser from slice to local state

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from './Modal'
 import '../css/Settings.css'
-import { updateUser, updateLocalUser, toggleSettingsModal } from "../features/globalSlice";
+import { updateUser, toggleSettingsModal } from "../features/globalSlice";
 import { connect, useDispatch } from 'react-redux'
 
 function Settings(props) {
   const dispatch = useDispatch()
+  const [user, setUser] = useState({
+    id: props.user.id,
+    fname: props.user.fname,
+    lname: props.user.lname,
+    budget: props.user.budget,
+    email: props.user.email,
+  })
 
   const handleSettingChange = (event) => {
     event.preventDefault();
 
-    let id = document.getElementById('id').value
-    let fname = document.getElementById('fname').value
-    let lname = document.getElementById('lname').value
-    let budget = document.getElementById('budget').value
-    let email = document.getElementById('email').value
+    let id = user.id;
+    let fname = user.fname;
+    let lname = user.lname;
+    let budget = user.budget;
+    let email = user.email;
 
     dispatch(updateUser({id, fname, lname, budget, email}));
     alert('Setting Changed!')
@@ -23,25 +30,31 @@ function Settings(props) {
   const handleChange = (event) => {
     const { id, value } = event.target;
 
-    dispatch(updateLocalUser({id, value}));
+    setUser(prevUser => {
+      return {
+        ...prevUser,
+        [id]: value
+      };
+    });
   }
 
   const closeSettingsModal = () => {
     dispatch(toggleSettingsModal(''))
   }
 
+  // props.localUser.id
   const profileForm =
     <form className='signup-form'>
       <label>ID</label>
-      <input type="text" id="id" value={props.localUser.id} onChange={handleChange}/>
+      <input type="text" id="id" value={user.id} onChange={handleChange}/>
       <label>First Name</label>
-      <input type="text" id="fname" value={props.localUser.fname} onChange={handleChange}/>
+      <input type="text" id="fname" value={user.fname} onChange={handleChange}/>
       <label>Last Name</label>
-      <input type="text" id="lname" value={props.localUser.lname} onChange={handleChange}/>
+      <input type="text" id="lname" value={user.lname} onChange={handleChange}/>
       <label>Budget</label>
-      <input type="number" id="budget" value={props.localUser.budget} onChange={handleChange}/>
+      <input type="number" id="budget" value={user.budget} onChange={handleChange}/>
       <label>Email</label>
-      <input type="email" id="email" value={props.localUser.email} onChange={handleChange}/>
+      <input type="email" id="email" value={user.email} onChange={handleChange}/>
       <label>Password</label>
       <input type="password" />
       <label>Confirm Password</label>
@@ -61,8 +74,7 @@ function Settings(props) {
 const mapStateToProps = (state) => {
   return {
     showSettings: state.global.showSettingsModal,
-    user: state.global.user,
-    localUser: state.global.localUser,
+    user: state.global.user
   }
 }
 
--- src/features/globalSlice.js
+++ src/features/globalSlice.js
@@ -11,13 +11,6 @@ const globalSlice = createSlice({
       budget: 0,
       email: 'budgeto@gmail.com',
     },
-    localUser: {
-      id: 'budgeto.io',
-      fname: 'Kevin',
-      lname: 'Lee',
-      budget: 0,
-      email: 'budgeto@gmail.com',
-    },
     showLoginModal: '',  // can be: login, signup, or ''
     showSettingsModal: '' // can be: settings or ''
   },
@@ -39,31 +32,6 @@ const globalSlice = createSlice({
       state.user.email = action.payload.email;
       state.showSettingsModal = '';
     },
-    updateLocalUser: (state, action) => {
-      console.log('hit updateLocalUser action')
-
-      let inputId = action.payload.id
-      let value = action.payload.value
-      switch(inputId) {
-        case "id":
-          state.localUser.id = value;
-          break;
-        case "fname":
-          state.localUser.fname = value;
-          break;
-        case "lname":
-          state.localUser.lname = value;
-          break;
-        case "email":
-          state.localUser.email = value;
-          break;
-        case "budget":
-          state.localUser.budget = value;
-          break;
-        default:
-          break;
-      }
-    },
     toggleLoginModal: (state, action) => {
       console.log('hit toggleLoginModal action')
       state.showLoginModal = action.payload
@@ -75,6 +43,6 @@ const globalSlice = createSlice({
   }
 })
 
-export const { userLogin, userLogout, updateUser, updateLocalUser, toggleLoginModal, toggleSettingsModal } = globalSlice.actions
+export const { userLogin, userLogout, updateUser, toggleLoginModal, toggleSettingsModal } = globalSlice.actions
 
 export default globalSlice.reducer
