8526d6197b81e2fd83457b7135bd00dbea69b02a
chntlc <chantaalchung@gmail.com>
2021-07-11 00:16:55 -0700

    build out query for dashboard

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const dashboardRouter = require('./routes/dashboard');
 
 var app = express();
+
+// setup CORS
+app.use(cors());
+app.options('*', cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
