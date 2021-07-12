fcdbc9926a4009a58ba1c58f11a7a1e59c96e92b
tmdenddl <seungwook.l95@gmail.com>
2021-07-11 23:44:03 -0700

    Basic passport setup

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,39 +3,30 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const userSchema = require("../schemas/Users");
 const bcrypt = require("bcrypt");
-
 const saltRounds = 10;
-
 // Level 2: Database Encryption
 // const encrypt = require("mongoose-encryption");
 // const secret = "Thisisourlittlesecret.";
 // userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
-
 // Level 3: Hashing Password
 // const md5 = require("md5");
 // Remember to wrap any password reference with md5();
-
 // Level 4: Salting and Hashing
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
-
 const User = mongoose.model("User", userSchema);
-
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log("This is GET method to /users.");
   console.log("This is what you have requested: ", req.body);
-
   res.send(false);
 });
-
-router.post('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
   console.log("This is POST method to /user/login");
   console.log("This is what you have requested: ", req.body);
-
-  User.findOne({email: req.body.email}, function(err, foundUser) {
+  User.findOne({ email: req.body.email }, function (err, foundUser) {
     console.log("This is the found User: ", foundUser);
     if (foundUser) {
-      bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+      bcrypt.compare(req.body.password, foundUser.password, function (err, result) {
         if (result === true) {
           const filteredUser = {
             _id: foundUser._id,
@@ -49,20 +40,17 @@ router.post('/login', function(req, res, next) {
           res.status(400).send("Incorrect user credential provided!");
         }
       });
-
     }
   })
-  .catch(err => {
-    console.log(err);
-    res.status(400).send("Bad Requests!");
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(400).send("Bad Requests!");
+    });
 });
-
-router.post('/signup', function(req, res, next) {
+router.post('/signup', function (req, res, next) {
   console.log("This is POST method to /users/signup");
   console.log("This is what you have requested: ", req.body);
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
     const newUser = new User({
       email: req.body.email,
       fname: req.body.fname,
@@ -77,15 +65,12 @@ router.post('/signup', function(req, res, next) {
         "60ea8555bc80e195b3f3f6a3"
       ]
     });
-
     console.log("This is the newUser to be saved: ", newUser);
-
     newUser.save()
       .then(savedUser => {
         console.log("This is the saved User: ", savedUser);
-
         const returningFields = ['_id', 'fname', 'lname', 'email', 'budget', 'category_ids'];
-        User.findById(savedUser.id, returningFields, function(err, docs) {
+        User.findById(savedUser.id, returningFields, function (err, docs) {
           console.log("This is the saved User with specified fields: ", docs);
           res.json(docs);
         });
@@ -97,15 +82,15 @@ router.post('/signup', function(req, res, next) {
       });
   });
 });
-
-router.patch("/settings", function(req, res, next) {
+router.patch("/settings", function (req, res, next) {
   const userToChange = req.body;
   console.log("This is PATCH method to /users/settings");
   console.log("This is what you have requested: ", userToChange);
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
     User.findOneAndUpdate(
-      {_id: userToChange._id},
-      {$set:
+      { _id: userToChange._id },
+      {
+        $set:
         {
           email: req.body.email,
           fname: req.body.fname,
@@ -114,24 +99,21 @@ router.patch("/settings", function(req, res, next) {
           password: hash,
         }
       },
-      {returnOriginal:false}
+      { returnOriginal: false }
     )
-    .then(updatedUser => {
-      console.log("This is the updated User: ", updatedUser);
-
-      const returningFields = ['_id', 'fname', 'lname', 'email', 'budget', 'category_ids'];
-      User.findById(updatedUser.id, returningFields, function(err, docs) {
-        console.log("This is the updated User with specified fields: ", docs);
-        res.json(docs);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).send("Bad Requests!");
-        // res.redirect(400, "http://localhost:3000/dashboard");
+      .then(updatedUser => {
+        console.log("This is the updated User: ", updatedUser);
+        const returningFields = ['_id', 'fname', 'lname', 'email', 'budget', 'category_ids'];
+        User.findById(updatedUser.id, returningFields, function (err, docs) {
+          console.log("This is the updated User with specified fields: ", docs);
+          res.json(docs);
+        })
+          .catch(err => {
+            console.log(err);
+            res.status(400).send("Bad Requests!");
+            // res.redirect(400, "http://localhost:3000/dashboard");
+          });
       });
-    });
   });
-
 });
-
 module.exports = router;
--- backend/schemas/Users.js
+++ backend/schemas/Users.js
@@ -23,30 +23,5 @@ const userSchema = new mongoose.Schema({
   versionKey: false
 });
 
-// const userSchema = mongoose.Schema({
-//   id: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   fname: String,
-//   lname: String,
-//   email: {
-//     type: String,
-//     lowercase: true,
-//     unique: true,
-//     required: [true, "can't be blank"],
-//     validate: (value) => {
-//       return validator.isEmail(value);
-//     },
-//   },
-//   budget: Number,
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-//   category_ids: [String],
-// });
-
 // module.exports = mongoose.model("User", schema);
 module.exports = userSchema;
