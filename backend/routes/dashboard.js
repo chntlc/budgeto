46eed12273c265b113bc686b092b77b4204d58de
chntlc <chantaalchung@gmail.com>
2021-07-12 00:20:12 -0700

    return proper response from endpoint

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const Item = require('../schemas/Items');
+const Category = require('../schemas/Categories');
 
 // TODO: build out query, test
 
 /* GET most spent category for a specified week */
-router.get('/dashboard/category/:user_id', function (req, res, next) {
+router.get('/category/:user_id', function (req, res, next) {
+  console.log({ params: req.params }, { query: req.query });
   const user_id = req.params.user_id;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
   // look at user collection, find all category_ids for user
   // look in item collection, find all items entered for specified date range, group items by category_id
   // for each item, sum up price * quantitiy
@@ -37,22 +42,73 @@ router.get('/dashboard/category/:user_id', function (req, res, next) {
   //     },
   //   }
   // ])
-  res.send('respond with most spent category');
+
+  Item.aggregate([
+    {
+      "$match": {
+        user_id: user_id,
+        date: {
+          $gte: new Date(startDate),
+          $lt: new Date(endDate)
+        }
+      }
+    },
+    {
+      "$group": {
+        _id: '$category_id',
+        total: {
+          $sum: {
+            $multiply: ['$price', '$qty']
+          }
+        }
+      }
+    },
+    {
+      "$sort": {
+        total: -1
+      }
+    }
+  ])
+    .then((items) => {
+      console.log({ items });
+      // sorted by -1 so highest value is always first in array
+      const categoryId = items[0]._id;
+      const total = parseFloat(items[0].total);
+
+      console.log({ categoryId });
+
+      Category.find({
+        _id: categoryId
+      })
+        .select('name')
+        .then((item) => {
+          console.log({ item })
+          const mostSpent = {
+            name: item[0].name,
+            total
+          }
+          res.send(mostSpent);
+        })
+        .catch((err) => {
+          console.log({ err });
+          res.send(err);
+        })
+    })
+    .catch((err) => {
+      console.log({ err });
+      res.send(err);
+    });
+  // res.send('respond with most spent category');
 });
 
 /* GET amount spent for the week */
 router.get('/budget/:user_id', function (req, res, next) {
-  console.log({ params: req.params }, { query: req.query })
+  console.log({ params: req.params }, { query: req.query });
   const user_id = req.params.user_id;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
   // look in item collection, find all items entered for specified date range, group items by category_id
-  Item.find()
-    .then((items) => {
-      console.log({ items });
-      res.send(items);
-    })
-    .catch((err) => {
-      console.log({ err })
-    })
 
   // this is basically the query i need to do, but with dynamic fields i get
   // gets total amount items, price spend for user_id -- can put NULL cuz redundant
@@ -80,17 +136,38 @@ router.get('/budget/:user_id', function (req, res, next) {
   //   }
   // ])
 
-  Item.aggregate([{
-    "$match": {
-      user_id: user_id
+  Item.aggregate([
+    {
+      "$match": {
+        user_id: user_id,
+        date: {
+          $gte: new Date(startDate),
+          $lt: new Date(endDate)
+        }
+      }
+    },
+    {
+      "$group": {
+        _id: null,
+        total: {
+          $sum: {
+            $multiply: ['$price', '$qty']
+          }
+        }
+      }
     }
-  },
-  {
-    "$group": {
-      _id: 'category_id'
-    }
-  }
   ])
+    .then((items) => {
+      console.log({ items });
+      const total = {
+        total: parseFloat(items[0].total)
+      }
+      res.send(total);
+    })
+    .catch((err) => {
+      console.log({ err });
+      res.send(err);
+    });
   // for each item, sum up price * quantitiy
   // return total amount spent for period
   // res.send('respond with amount spent for week');
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,47 +1,54 @@
 import React from "react";
 import DashboardBtn from "./DashboardBtn";
 import "../css/Dashboard.css";
+import { connect, useDispatch } from 'react-redux';
 
-class DashboardContent extends React.Component {
-  render() {
-    const userName = "Anonymous",
-      userImg =
-        "https://cdn1.iconfinder.com/data/icons/social-black-buttons/512/anonymous-512.png",
-      userBudget = 300,
-      userSpending = 256,
-      moneyDiff = userBudget - userSpending,
-      mostSpentCat = "Food";
-    return (
-      <div className="dashboard page-content">
-        <div className="dashboard__content">
-          <div className="dashboard__greeting">
-            <img src={userImg} alt="UserImg" />
-            <span>Hi, {userName}!</span>
-          </div>
-          <div className="dashboard__report">
-            <p>
-              You've spent <strong className="green">${userSpending}</strong>{" "}
+function DashboardContent(props) {
+  const userName = "Anonymous",
+    userImg =
+      "https://cdn1.iconfinder.com/data/icons/social-black-buttons/512/anonymous-512.png",
+    userBudget = 300,
+    userSpending = 256,
+    moneyDiff = userBudget - userSpending,
+    mostSpentCat = "Food";
+  return (
+    <div className="dashboard page-content">
+      <div className="dashboard__content">
+        <div className="dashboard__greeting">
+          <img src={userImg} alt="UserImg" />
+          <span>Hi, {userName}!</span>
+        </div>
+        <div className="dashboard__report">
+          <p>
+            You've spent <strong className="green">${userSpending}</strong>{" "}
               this week
             </p>
-            <p>
-              You're <strong className="red">${Math.abs(moneyDiff)}</strong>{" "}
-              {moneyDiff > 0 ? "under" : "over"} your budget of{" "}
-              <strong className="brown">${userBudget}</strong>
-            </p>
-            <p>
-              Your most spent category is:{" "}
-              <strong className="blue">{mostSpentCat}</strong>
-            </p>
-          </div>
-        </div>
-        <hr className="dashboard__border" />
-        <div className="dashboard__actions">
-          <DashboardBtn label="Add Spending" path="/add" />
-          <DashboardBtn label="View Spending" path="/view" />
+          <p>
+            You're <strong className="red">${Math.abs(moneyDiff)}</strong>{" "}
+            {moneyDiff > 0 ? "under" : "over"} your budget of{" "}
+            <strong className="brown">${userBudget}</strong>
+          </p>
+          <p>
+            Your most spent category is:{" "}
+            <strong className="blue">{mostSpentCat}</strong>
+          </p>
         </div>
       </div>
-    );
+      <hr className="dashboard__border" />
+      <div className="dashboard__actions">
+        <DashboardBtn label="Add Spending" path="/add" />
+        <DashboardBtn label="View Spending" path="/view" />
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    showLogin: state.global.,
+    user: state.global.user,
+    isLoggedIn: state.global.isLoggedIn
   }
 }
 
-export default DashboardContent;
+export default connect(mapStateToProps)(DashboardContent);
