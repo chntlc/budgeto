23032836ddd9f24d9f209f8242fc60c80fa5556c
tmdenddl <seungwook.l95@gmail.com>
2021-07-13 00:58:30 -0700

    Creating User in the DB

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var signupRouter = require('./routes/signup');
+var settingsRouter = require('./routes/settings');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
+app.use('/settings', settingsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/schemas/Users.js
+++ backend/schemas/Users.js
@@ -2,27 +2,51 @@ const mongoose = require("mongoose");
 let validator = require("validator");
 
 const userSchema = mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  fname: String,
+  lname: String,
   email: {
     type: String,
     lowercase: true,
+    unique: true,
+    required: [true, "can't be blank"],
     validate: (value) => {
       return validator.isEmail(value);
     },
   },
-  fname: String,
-  lname: String,
   budget: Number,
   password: {
     type: String,
-    required: true,
+    required: [true, "can't be blank"],
   },
   category_ids: [String],
+}, {
+  versionKey: false
 });
+
+// const userSchema = mongoose.Schema({
+//   id: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   fname: String,
+//   lname: String,
+//   email: {
+//     type: String,
+//     lowercase: true,
+//     unique: true,
+//     required: [true, "can't be blank"],
+//     validate: (value) => {
+//       return validator.isEmail(value);
+//     },
+//   },
+//   budget: Number,
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   category_ids: [String],
+// });
 
 // module.exports = mongoose.model("User", schema);
 module.exports = userSchema;
