9561361e5286347c5937518d91899b4fb65ebda1
chntlc <chantaalchung@gmail.com>
2021-07-26 14:02:36 -0700

    only update user for settings fields that are changed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -112,40 +112,44 @@ router.patch("/settings", function (req, res, next) {
   const userToChange = req.body;
   console.log("This is PATCH method to /users/settings");
   console.log("This is what you have requested: ", userToChange);
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+  const findUpdateUser = (hash) => {
+    // only update fields if they've changed
+    const $set = {};
+    if (req.body.email) $set.req.body.email;
+    if (req.body.fname) $set.req.body.fname;
+    if (req.body.lname) $set.req.body.lname;
+    if (req.body.budget) $set.req.body.budget;
+    if (req.body.password) $set.password = hash;
+
     User.findOneAndUpdate(
       { _id: userToChange._id },
       {
-        $set: {
-          email: req.body.email,
-          fname: req.body.fname,
-          lname: req.body.lname,
-          budget: req.body.budget,
-          password: hash,
-        },
+        $set
       },
-      { returnOriginal: false }
+      {
+        fields: { _id: 1, fname: 1, lname: 1, email: 1, budget: 1, category_ids: 1 },
+        returnOriginal: false
+      }
     ).then((updatedUser) => {
       console.log("This is the updated User: ", updatedUser);
 
-      const returningFields = [
-        "_id",
-        "fname",
-        "lname",
-        "email",
-        "budget",
-        "category_ids",
-      ];
-      User.findById(updatedUser.id, returningFields, function (err, docs) {
-        console.log("This is the updated User with specified fields: ", docs);
-        res.json(docs);
-      }).catch((err) => {
+      res.json(updatedUser);
+    })
+      .catch((err) => {
         console.log(err);
         res.status(400).send("Bad Requests!");
         // res.redirect(400, "http://localhost:3000/dashboard");
       });
+  }
+
+  if (req.body.password) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      findUpdateUser(hash)
     });
-  });
+  } else {
+    findUpdateUser()
+  }
 });
 
 module.exports = router;
