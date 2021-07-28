ee8905e6b262c7d65f1ea1e0a944b076aa71af57
tmdenddl <seungwook.l95@gmail.com>
2021-07-27 20:01:08 -0700

    Deploy

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/dashboard", dashboardRouter);
 app.use("/view", viewRouter);
