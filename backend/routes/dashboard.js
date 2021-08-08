20515602dba324c9231201c3c526c0a1822a85de
chntlc <chantaalchung@gmail.com>
2021-08-08 02:02:06 -0700

    cleanup, put correct category id in default

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -4,21 +4,11 @@ const router = express.Router();
 const Item = require('../schemas/Items');
 const Category = require('../schemas/Categories');
 
-// TODO: build out query, test
-
 /* GET most spent category for a specified week */
 router.get('/category/:user_id', function (req, res, next) {
-  console.log({ params: req.params }, { query: req.query });
   const user_id = req.params.user_id;
   const startDate = req.query.startDate;
   const endDate = req.query.endDate;
-
-  // look at user collection, find all category_ids for user
-  // look in item collection, find all items entered for specified date range, group items by category_id
-  // for each item, sum up price * quantitiy
-  // get total sum per category
-  // get highest total sum
-  // look in category collection for category name, given category_id
 
   Item.aggregate([
     {
@@ -47,19 +37,15 @@ router.get('/category/:user_id', function (req, res, next) {
     }
   ])
     .then((items) => {
-      console.log({ items });
       // sorted by -1 so highest value is always first in array
       const categoryId = items[0]._id;
       const total = parseFloat(items[0].total);
-
-      console.log({ categoryId });
 
       Category.find({
         _id: categoryId
       })
         .select('name')
         .then((item) => {
-          console.log({ item })
           const mostSpent = {
             name: item[0].name,
             total
@@ -79,17 +65,9 @@ router.get('/category/:user_id', function (req, res, next) {
 
 /* GET amount spent for the week */
 router.get('/budget/:user_id', function (req, res, next) {
-  console.log({ params: req.params }, { query: req.query });
   const user_id = req.params.user_id;
   const startDate = req.query.startDate;
   const endDate = req.query.endDate;
-
-  // look in item collection, find all items entered for specified date range
-  // for each item, sum up price * quantitiy
-  // return total amount spent for period
-  // res.send('respond with amount spent for week');
-  // gets total amount items, price spend for user_id -- can put NULL cuz redundant
-  // already used user_id in MATCH
 
   Item.aggregate([
     {
@@ -113,7 +91,6 @@ router.get('/budget/:user_id', function (req, res, next) {
     }
   ])
     .then((items) => {
-      console.log({ items });
       const spent = {
         spent: parseFloat(items[0].total)
       }
