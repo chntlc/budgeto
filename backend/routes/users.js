3476f561d2d25699ce87860d6f86532e1f6adab3
tmdenddl <seungwook.l95@gmail.com>
2021-08-08 20:26:29 -0700

    Don't really need 401 status code

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -158,7 +158,6 @@ router.post("/refreshToken", (req, res, next) => {
               (item) => item.refreshToken === refreshToken
             );
             if (tokenIndex === -1) {
-              res.statusCode = 401;
               res.send("Unauthorized");
             } else {
               const token = getToken({ _id: userId });
@@ -185,18 +184,15 @@ router.post("/refreshToken", (req, res, next) => {
               });
             }
           } else {
-            res.statusCode = 401;
             res.send("Unauthorized");
           }
         },
         (err) => next(err)
       );
     } catch (err) {
-      res.statusCode = 401;
       res.send("Unauthorized");
     }
   } else {
-    res.statusCode = 401;
     res.send("Unauthorized");
   }
 });
